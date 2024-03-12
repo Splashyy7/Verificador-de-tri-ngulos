67c5684dbaccf26152060da3027948208e4fbc08
João Pedro Oliveira Gritz <135381904+Splashyy7@users.noreply.github.com>
2024-03-12 08:02:24 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+let lado1= prompt("Informe o primeiro lado do triângulo");
+let lado2= prompt("Informe o segundo lado do triângulo");
+let lado3= prompt("Informe o terceiro lado do triângulo");
+
+if (lado1 + lado2 < lado3){
+  alert("Pode formar um triângulo")
+} else { 
+ 
+  alert("Não pode formar um triângulo")
+}
